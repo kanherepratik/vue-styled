9cd9f0369410e565e0da99a534ae5010ef52b6b7
prateek kanhere <kanherepratik@gmail.com>
2020-04-15 23:19:13 +0530

    lint setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,119 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-    '@vue/typescript/recommended',
-  ],
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
+    extends: [
+        'airbnb/base',
+        'prettier/@typescript-eslint',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:vue/recommended',
+        '@vue/airbnb',
+        '@vue/typescript/recommended',
+        'plugin:prettier/recommended',
+    ],
+    "parserOptions": {
+        parser: '@typescript-eslint/parser',
+        "ecmaVersion": 2020,
+        "sourceType": "module"
+     },
+    plugins: ['@typescript-eslint'],
+    rules: {
+        'no-unused-vars': 'off',
+        'no-useless-constructor': 'off',
+        "indent": 'off',
+        'no-tabs': 'error',
+        'max-len': [2, 140, 4, { ignoreUrls: true }],
+        'no-template-curly-in-string': 'error',
+        '@typescript-eslint/indent': 'error',
+        'prefer-arrow-callback': 'error',
+        '@typescript-eslint/no-unused-vars': 'error',
+        '@typescript-eslint/no-useless-constructor': 'error',
+        '@typescript-eslint/default-param-last': 'error',
+        '@typescript-eslint/explicit-function-return-type': 'error',
+        '@typescript-eslint/no-empty-interface': 'error',
+        '@typescript-eslint/no-explicit-any': 'error',
+        '@typescript-eslint/no-implied-eval': 'error',
+        '@typescript-eslint/no-inferrable-types': 'error',
+        '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'error',
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/naming-convention': [
+            'error',
+            {
+                selector: 'default',
+                format: ['camelCase']
+            },
+
+            {
+                selector: 'variable',
+                format: ['camelCase', 'UPPER_CASE']
+            },
+            {
+                selector: 'parameter',
+                format: ['camelCase'],
+                leadingUnderscore: 'allow'
+            },
+
+            {
+                selector: 'memberLike',
+                modifiers: ['private'],
+                format: ['camelCase'],
+                leadingUnderscore: 'require'
+            },
+
+            {
+                selector: 'typeLike',
+                format: ['PascalCase']
+            },
+            {
+                selector: 'class',
+                format: ['PascalCase']
+            },
+            {
+                selector: 'interface',
+                format: ['PascalCase'],
+                prefix: ['I']
+            },
+            {
+                selector: 'enum',
+                format: ['PascalCase']
+            }
+        ],
+        'require-jsdoc': [
+            'error',
+            {
+                require: {
+                    FunctionDeclaration: true,
+                    MethodDefinition: false,
+                    ClassDeclaration: false,
+                    ArrowFunctionExpression: false,
+                    FunctionExpression: false
+                }
+            }
+        ],
+        'no-console': 'warn',
+        'no-debugger': 'warn',
+        "vue/html-indent": ["error", 4,
+         {"attribute": 1, "closeBracket": 1, 
+            "alignAttributesVertically": true}],
+        "vue/singleline-html-element-content-newline": ["error", {
+            "ignoreWhenNoAttributes": true,
+            "ignoreWhenEmpty": true,
+        }]
+        
+    },
+    settings: {
+        'import/extensions': ['.js', '.ts'],
+        'import/parsers': {
+            '@typescript-eslint/parser': ['.ts', '.tsx']
+        },
+        'import/resolver': {
+            node: {
+                extensions: ['.js', '.jsx', '.ts', '.tsx']
+            }
+        }
+    },
+    parserOptions: {
+        ecmaVersion: 2020,
+        tsconfigRootDir: '.',
+        project: ['./tsconfig.json']
+    }
 };
